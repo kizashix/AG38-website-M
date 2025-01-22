381c721399d428cf6522d6c8e82661564fe441f0
AMS <kizashix@users.noreply.github.com>
2025-01-22 01:55:33 +0000

    Cyberpunk profile page

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// JavaScript Interaction: Button Click Event
+document.getElementById('learn-more-btn').addEventListener('click', function () {
+    alert('Welcome to Cyber Security Expert! Let’s secure your digital future.');
+  });
+  
